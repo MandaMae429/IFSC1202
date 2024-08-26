be7b6f3318396467c60aec90a48e527f5f4cc186
MandaMae429 <amyers1@ualr.edu>
2024-08-26 15:28:26 +0000

    Week 2

---
+++ 02.01 Last Digit.py
@@ -0,0 +1,11 @@
+number=23
+print("Enter a number:","{}".format(number))
+print("Last Digit:","{}".format(number % 10) )
+
+number=5
+print("Enter a number:","{}".format(number))
+print("Last Digit:","{}".format(number % 10) )
+
+number=10
+print("Enter a number:","{}".format(number))
+print("Last Digit:","{}".format(number % 10) )
