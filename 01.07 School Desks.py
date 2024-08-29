049ee8fd3ceb9d42f9c83a428c90e824ff6966d1
MandaMae429 <amyers1@ualr.edu>
2024-08-29 01:55:42 +0000

    AMyers24

--- 01.07 School Desks.py
+++ 01.07 School Desks.py
@@ -1,3 +1,9 @@
 a=(int(input("Enter Classroom A: ")))
 b=(int(input("Enter Classroom B: ")))
 c=(int(input("Enter Classroom C: ")))
+sum1=(a//2)+(a%2)
+sum2=(b//2)+(b%2)
+sum3=(c//2)+(c%2)
+print (sum1+sum2+sum3)
+
+
---
+++ 01.08 Number of Seconds.py
@@ -0,0 +1,6 @@
+inputmin=(input("Enter Minutes: "))
+min=int(inputmin)
+inputsec=(input("Enter Seconds: "))
+sec=int(inputsec)
+min1=min*60+(sec)
+print (min1)
