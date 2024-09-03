37f8f160170dba53a992b2031d471b637aacd48e
MandaMae429 <amyers1@ualr.edu>
2024-09-03 16:57:55 +0000

    AMyers24

--- 01.10 Average Speed.py
+++ 01.10 Average Speed.py
@@ -1,5 +1,5 @@
 kilo=(int(input("Enter Length of Race in Kilometers: ")))
-min=(int(input("Enter Minutes: ")))*3600
+min=(int(input("Enter Minutes: ")))*60
 sec=(int(input("Enter Seconds: ")))
 kilo1=kilo/1.61
-print (int(kilo1/(min+sec)))
+print (float(kilo1/(min+sec))*3600)
