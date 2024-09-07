4868ed588b734383e2ea7dba07c827dd9b18be07
MandaMae429 <amyers1@ualr.edu>
2024-09-07 14:45:45 +0000

    AMyers24

---
+++ 02.09 Fractional Part.py
@@ -0,0 +1,4 @@
+num=(float(input("Enter a Number: ")))
+numfrac=num%1
+numfracr=(round(numfrac,10))
+print ("Fractional Part: ""{}".format(numfracr))
---
+++ 02.10 First Digit After Decimal.py
@@ -0,0 +1,5 @@
+num=(float(input("Enter Number: ")))
+numfrac=num%1
+numfracr=(round(numfrac,10))
+numfractens=(round(numfracr//.10))
+print ("Tenths Value: ""{}".format(numfractens))
