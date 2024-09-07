0ca0045741771841326a38ab22cef8bad9f2ddd8
MandaMae429 <amyers1@ualr.edu>
2024-09-07 13:50:51 +0000

    AMyers24

---
+++ 02.08 Merge Two Numbers.py
@@ -0,0 +1,7 @@
+num1=(int(input("Enter First 2 Digit Number: ")))
+num2=(int(input("Enter Second 2 Digit Number: ")))
+left1=(int(num1//10))
+right1=(int(num1%10))
+left2=(int(num2//10))
+right2=(int(num2%10))
+print ("Merged Number: : ""{}{}{}{}".format(left1,left2,right1,right2))
