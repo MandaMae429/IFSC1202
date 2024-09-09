ee83a8c0492f791f878ecd0e80e6b181faacc6da
MandaMae429 <amyers1@ualr.edu>
2024-09-09 15:19:22 +0000

    AMyers24

---
+++ 03.06 Index of Outlier.py
@@ -0,0 +1,11 @@
+a=(int(input("Enter First Number: ")))
+b=(int(input("Enter Second Number: ")))
+c=(int(input("Enter Third Number: ")))
+if a==c:
+    print ("2")
+elif a==b:
+    print ("3")
+elif b==c:
+    print ("1")
+else:
+    print
