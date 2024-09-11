77ec067a1e057e2d51ce1764e13788b2880115fd
MandaMae429 <amyers1@ualr.edu>
2024-09-11 16:54:55 +0000

    AMyers24

--- 03.09 Next Day.py
+++ 03.09 Next Day.py
@@ -1,2 +1,18 @@
 month=(int(input("Enter Month: ")))
-day=(int(input("Enter Day")))
+day=(int(input("Enter Day: ")))
+if month==2:
+    dlimit=28
+elif month==4 or month==6 or month==9 or month==11:
+    dlimit=30
+else:
+    dlimit=31
+if day>=dlimit:
+    day=1
+    if month>=12:
+        month=1
+    else:
+        month+=1
+else:
+    day+=1
+print ("Next Day: ",month,"/", day)
+ 
