c95559b615309b29a1fef8ec3feb6c14eaf2266e
MandaMae429 <amyers1@ualr.edu>
2024-09-18 15:54:54 +0000

    AMyers24

---
+++ 04.04 Number of Zeros.py
@@ -0,0 +1,4 @@
+n=(int(input("Enter N: ")))
+for i in range (n):
+    if n==0:
+        print (n)
---
+++ 04.05 Number of Factorials.py
@@ -0,0 +1,6 @@
+n=(int(input("Enter Number: ")))
+fact=1
+if n >= 1:
+    for x in range (1, n+1):
+        fact=fact+x
+        print (fact)
