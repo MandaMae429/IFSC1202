025a5b07df1247e8e783144301cc2b4240e1fa9a
MandaMae429 <amyers1@ualr.edu>
2024-09-18 15:00:10 +0000

    AMyers24

---
+++ 04.01 Product of N Numbers.py
@@ -0,0 +1,5 @@
+n=(int(input("Enter N: ")))
+prod=1
+for x in range(n):
+    prod=prod*(int(input("Enter Number: ")))
+print (prod)
---
+++ 04.02 Sum of Cubes.py
@@ -0,0 +1,5 @@
+n=(int(input("Enter Number: ")))
+sum=0
+for x in range(n):
+    sum=sum+((x+1)**3)
+print (sum)
