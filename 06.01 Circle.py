1d65f30a3322dcc96640dc03dddffa6626eb7f7c
MandaMae429 <amyers1@ualr.edu>
2024-09-25 15:39:50 +0000

    First Commit

--- 06.01 Circle.py
+++ 06.01 Circle.py
@@ -1,9 +1,4 @@
-n1=1.6
-#n2=2.93
-#n3=200.99
-#n4=1048.732
-import math
-radi=n1
-diam=(n1*2)
-circ=n1*math.pi*2
-print (radi, diam, circ)
+radiustextfile=open ("06.01 Radius.txt", "r")
+x=radiustextfile.read
+print (x)
+radiustextfile.close
