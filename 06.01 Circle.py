7b5ff0d350528f1bc01d24c512dcd7c136313db7
MandaMae429 <amyers1@ualr.edu>
2024-09-25 15:51:13 +0000

    Second Commit

--- 06.01 Circle.py
+++ 06.01 Circle.py
@@ -1,4 +1,12 @@
+import math
+def diameter(r):
+    return (r*2)
+def circumference(r):
+    return (r*math.pi*2)
+def area(r):
+    return (math.pi*r*r)
+
 radiustextfile=open ("06.01 Radius.txt", "r")
-x=radiustextfile.read
+x=radiustextfile.readline()
 print (x)
-radiustextfile.close
+radiustextfile.close()
