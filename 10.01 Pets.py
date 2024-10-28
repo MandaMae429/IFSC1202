b4aade1a6e600f5db81490980f68a8282bb4fd28
MandaMae429 <amyers1@ualr.edu>
2024-10-28 15:44:30 +0000

    Second Commit 10

--- 10.01 Pets.py
+++ 10.01 Pets.py
@@ -2,6 +2,15 @@
 #Name - name of pet
 #Type - type of pet, such as "Cat", "Dog", etc
 #Age - age of pet
+class Pets ():
+     def __init__(self, name, type, age):
 #The constructor for the Pet class should not have any arguments.
-#Read the 10.01 Pets.txt file, create three pet objects, one for each line, and print the object attributes for the pets using the .format method
+        self.Name=name
+        self.Type=type
+        self.Age=age
+#Read the 10.01 Pets.txt file, create three pet objects, one for each line, and print the object attributes for the pets using the .format 
+file=open("10.01 Pets.txt", "r")
+file.readline()
+
+print (file.readline())
 #Print the Name, Type, and Age attributes from each of the pet objects
--- 10.02 Retail Item.py
+++ 10.02 Retail Item.py
@@ -3,8 +3,21 @@
 #Description - description of the item (default "")
 #UnitsOnHand - the number of units in inventory (default 0)
 #Price - the item's retail price (default 0)
+class RetailItem ():
+    def __init__(self, description, unitsonhand=0, price=0):
+          
 #Create a constructor that accepts the Description, UnitOnHand, and Price, then sets them to attributes in the RetailItem Class.
+        self.Description=description
+        self.UnitsOnHand=unitsonhand
+        self.Price=price 
 #If one of the attributes is not present, then set the attribute to the default value.
+      
 #Create a method called InventoryValue, which returns the UnitsOnHand times Price.
+    def InventoryValue(self):
+        inventoryvalue= self.UnitsOnHand * self.Price
+        return inventoryvalue
+
 #Read the 10.02 Inventory.txt file and creates three objects, one for each item.
+file=open("10.02 Inventory.txt", "r")
+file.readline()
 #Display a report that displays the Description, Units On Hand, Price, and Inventory Value
